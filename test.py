2df779cd8bfa87c9311f7e73fb5adca8436ca7b8
asherhoffman1120 <asher.hoffman1120@gmail.com>
2023-08-07 03:50:23 -0500

    tested extracting

---
+++ test.py
@@ -0,0 +1,14 @@
+from PyPDF2 import PdfReader
+from pprint import pprint
+pdf_file_name = 'input.pdf'
+
+f = PdfReader(pdf_file_name)
+fields = f.get_fields()
+print('RESULT : ', fields)
+fdfinfo = dict((k, v.get('/V', '')) for k, v in fields.items())
+pprint(fdfinfo)
+
+with open('test.csv', 'w') as f2:
+    for key in fdfinfo.keys():
+        if type(key)==type("string") and type(str(fdfinfo[key]))==type("string"):
+            f2.write('"'+key+'","'+fdfinfo[key]+'"\n')
