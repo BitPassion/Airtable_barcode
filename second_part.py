a407f83cb61135bfed60a3d7959119686b25b07f
asherhoffman1120 <asher.hoffman1120@gmail.com>
2023-08-08 03:23:17 -0500

    find the record_id

--- second_part.py
+++ second_part.py
@@ -2,7 +2,8 @@ from pdf2image import convert_from_path
 from PIL import Image
 from pyzbar.pyzbar import decode
 import cv2
-import numpy as np  # Import NumPy
+import numpy as np  
+from airtable import Airtable
 
 # Store Pdf with convert_from_path function
 images = convert_from_path('output.pdf', poppler_path=r'C:\Program Files\poppler\bin')
@@ -33,3 +34,28 @@ def barcodeReader(image):
 
 barcode = barcodeReader(im1_np)
 print(barcode)
+
+# Replace with your Airtable API credentials and base ID
+API_KEY = 'patsKa7ue00yT7O5z.ab8ac81f77631852c08df2c18fec20e0b8785126f563f23a6b47e10e70082c58'
+BASE_ID = 'app9TEsn6f0IFjNup'
+TABLE_NAME = 'tblUha0m2h1hGDF3A'
+
+# Initialize the Airtable client
+airtable = Airtable(BASE_ID, TABLE_NAME, api_key=API_KEY)
+
+# Retrieve all records from the table
+all_records = airtable.get_all()
+
+# Extract barcode values from each record
+barcode_values = []
+
+for record in all_records:
+    barcode_field = record['fields'].get('Barcode')
+    if barcode_field and barcode_field.get('text') == barcode[:-1]:
+        name = record['fields'].get('Name', '')
+        record_id = record['id']
+
+        print(f"Record ID: {record_id}")
+        print(f"Name: {name}")
+        print("----------")
+        break
