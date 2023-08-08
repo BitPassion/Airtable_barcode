0ed0348850d90cb9f2619fe75db12b984e8ae9f3
asherhoffman1120 <asher.hoffman1120@gmail.com>
2023-08-07 21:04:48 -0500

    barcode extracting finished

--- extract_barcode.py
+++ extract_barcode.py
@@ -1,13 +1,35 @@
-#from an image, extract barcode.
-
+from pdf2image import convert_from_path
+from PIL import Image
 from pyzbar.pyzbar import decode
 import cv2
+import numpy as np  # Import NumPy
 
-def barcodeReader(url):
-    image = cv2.imread(url)
-    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Store Pdf with convert_from_path function
+images = convert_from_path('output.pdf', poppler_path=r'C:\Program Files\poppler\bin')
+
+im = images[1]
+width, height = im.size
+
+# Setting the points for cropped image
+left = 1370
+top = 1920
+right = 1630
+bottom = 2060
+
+# Cropped image of above dimension
+# (It will not change original image)
+im1 = im.crop((left, top, right, bottom))
+
+# Shows the image in image viewer
+# im1.show()
+
+# Convert PIL image to NumPy array
+im1_np = np.array(im1)
+
+def barcodeReader(image):
+    gray_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)  # Convert to grayscale
     barcodes = decode(gray_img)
     return barcodes[0].data.decode('utf-8')
 
-barcode = barcodeReader("barcode_img.png")
-print (barcode)
+barcode = barcodeReader(im1_np)
+print(barcode)
--- ocr.py
+++ ocr.py
@@ -1,45 +1,7 @@
-# import fitz
-# import io
-# from PIL import Image
-  
-# # STEP 2
-# # file path you want to extract images from
-# file = "output.pdf"
-  
-# # open the file
-# pdf_file = fitz.open(file)
-  
-# # STEP 3
-# # iterate over PDF pages
-# for page_index in range(len(pdf_file)):
-  
-#     # get the page itself
-#     page = pdf_file[page_index]
-#     image_list = page.getImageList()
-  
-#     # printing number of images found in this page
-#     if image_list:
-#         print(
-#             f"[+] Found a total of {len(image_list)} images in page {page_index}")
-#     else:
-#         print("[!] No images found on page", page_index)
-#     for image_index, img in enumerate(page.getImageList(), start=1):
-  
-#         # get the XREF of the image
-#         xref = img[0]
-  
-#         # extract the image bytes
-#         base_image = pdf_file.extractImage(xref)
-#         image_bytes = base_image["image"]
-  
-#         # get the image extension
-#         image_ext = base_image["ext"]
-
-
-
-
 # import module
 from pdf2image import convert_from_path
+# Importing Image class from PIL module
+from PIL import Image
  
  
 # Store Pdf with convert_from_path function
@@ -51,3 +13,25 @@ images = convert_from_path('output.pdf', poppler_path = r'C:\Program Files\poppl
 #     images[i].save('page'+ str(i) +'.jpg', 'JPEG')
 
 images[1].save('captured.jpg','JPEG')
+
+
+# Opens a image in RGB mode
+im = Image.open(r"captured.jpg")
+# im = images[1].save('captured.jpg','JPEG')
+ 
+# Size of the image in pixels (size of original image)
+# (This is not mandatory)
+width, height = im.size
+ 
+# Setting the points for cropped image
+left = 1370
+top = 1920
+right = 1630
+bottom = 2060
+ 
+# Cropped image of above dimension
+# (It will not change original image)
+im1 = im.crop((left, top, right, bottom))
+ 
+# Shows the image in image viewer
+im1.show()
